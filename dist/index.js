4e33f31f1927c8075ef15015a874156d74ba59fd
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2021-07-25 14:13:55 +0100

    Basic Discord bot

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,8 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+client.login(process.env.DISCORD_TOKEN);
 //# sourceMappingURL=index.js.map
