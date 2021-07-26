9650a2ec3be3ba609fa5bec8c649ef10d33a4885
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2021-07-26 11:42:20 +0100

    Maps, checks, errors and caches - oh my!

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,44 @@ import Discord from "discord.js";
 const client = new Discord.Client();
 const prefix = "p!";
 const prefixRegex = new RegExp(`^${prefix}`);
+const registry = {
+    commands: new Map(),
+};
+const commandNameCache = new Map();
+function createCache(data, options) {
+    const cache = new Map();
+    let index = 0;
+    for (const item of data) {
+        if (!item[options.key] || !item[options.value]) {
+            throw new Error(`Found an object (index ${index}) that does not have a property that can be used as a key/value`);
+        }
+        cache.set(item[options.key], item[options.value]);
+        index++;
+    }
+    return cache;
+}
+function validNamespacedId(value) {
+    const validChars = /[a-z0-9_:]/g;
+    if (value.replace(validChars, "") === "") {
+        return true;
+    }
+    return false;
+}
 class Command {
-    constructor(name, params) {
+    constructor(name, id, callback, params = 0) {
         this.name = name;
         this.params = params;
+        this.id = id;
+        this.callback = callback;
+        if (name.length > 16) {
+            throw new Error("Command name lengths must be below 16 characters");
+        }
+        if (!validNamespacedId(id)) {
+            throw new Error("Namespaced IDs must only contain characters a-z, 0-9, _ or :");
+        }
+        if (callback.length > 1) {
+            throw new Error("Command callbacks can only take up to one parameter");
+        }
     }
 }
 client.on("ready", () => {
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,99 @@ import { Client } from "discord.js";
 import Discord from "discord.js";
 const client: Client = new Discord.Client();
 
+interface registry {
+  commands: Map<string, Command>;
+}
+
 const prefix = "p!";
 const prefixRegex = new RegExp(`^${prefix}`);
 
-class Command {
-  name: string;
-  params: number;
+const registry: registry = {
+  commands: new Map(),
+};
 
-  constructor(name: string, params: number) {
+/** A map of command names to command IDs. Used for quick lookup of which command a user has entered. */
+const commandNameCache: Map<string, string> = new Map();
+
+/**
+ * Generates a map that allows for quick lookup of a specific property
+ * @param data An object that has the data that needs to be cached
+ * @param options Specifies which properties of the above object should be used as the key and value when creating the cache
+ */
+function createCache(
+  data: any[],
+  options: {
+    key: string;
+    value: string;
+  }
+) {
+  const cache = new Map();
+
+  let index = 0;
+  for (const item of data) {
+    // If the item does not contain the specified properties,
+    // raise an error.
+    if (!item[options.key] || !item[options.value]) {
+      throw new Error(
+        `Found an object (index ${index}) that does not have a property that can be used as a key/value`
+      );
+    }
+
+    // Add this item to the cache
+    cache.set(item[options.key], item[options.value]);
+
+    index++;
+  }
+
+  return cache;
+}
+
+function validNamespacedId(value: string) {
+  const validChars = /[a-z0-9_:]/g;
+
+  // Removes every valid character form the string,
+  // then checks if the string is empty.
+  // There must be a better way to do this.
+  if (value.replace(validChars, "") === "") {
+    return true;
+  }
+
+  return false;
+}
+
+class Command {
+  // Properties
+  name;
+  params;
+  id;
+  callback;
+
+  /**
+   * Creates a new `Command` object.
+   * Note that this doesn't automatically register the command.
+   * @param name The name of the command. This is what the user types to execute the command.
+   * @param id A unique namespaced ID for the command.
+   * @param callback The function to be run when a user executes the command.
+   * @param params How many parameters should the command take?
+   */
+  constructor(name: string, id: string, callback: Function, params = 0) {
     this.name = name;
     this.params = params;
+    this.id = id;
+    this.callback = callback;
+
+    // This is meant to be far above what anyone would need
+    if (name.length > 16) {
+      throw new Error("Command name lengths must be below 16 characters");
+    }
+    if (!validNamespacedId(id)) {
+      throw new Error(
+        "Namespaced IDs must only contain characters a-z, 0-9, _ or :"
+      );
+    }
+    if (callback.length > 1) {
+      throw new Error("Command callbacks can only take up to one parameter");
+    }
   }
 }
 
